d75449874dcebb79d728b77f0a7f2d6b1a50de90
Yuri <yurimkrtumyan02@gmail.com>
2022-08-10 23:01:03 +0400

    buy sell

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,16 +1,48 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract Token {
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Token is Ownable {
     //mint burn test
 
     string private name;
     string private symbol;
     uint256 private totalSupply;
+    uint256 public tokenPrice = 2 wei;
+
     //solves division problem in solidity
     uint8 private decimals = 18;
     mapping(address => uint256) private balances;
     mapping(address => mapping(address => uint256)) private allowances;
+    mapping(address => bool) private whiteList;
+
+    modifier onlyWhitelisted() {
+        require(whiteList[msg.sender]);
+        _;
+    }
+
+    function addAddressToWhitelist(address addr)
+        public
+        onlyOwner
+        returns (bool success)
+    {
+        if (!whiteList[addr]) {
+            whiteList[addr] = true;
+            success = true;
+        }
+    }
+
+    function removeAddressFromWhitelist(address addr)
+        public
+        onlyOwner
+        returns (bool success)
+    {
+        if (whiteList[addr]) {
+            whiteList[addr] = false;
+            success = true;
+        }
+    }
 
     function getName() public view returns (string memory) {
         return name;
@@ -35,17 +67,15 @@ contract Token {
     function allowance(address _owner, address _spender)
         public
         view
-        returns (uint256 remaining)
+            returns (uint256 remaining)
     {
         return allowances[_owner][_spender];
     }
 
     event Transfer(address indexed _from, address indexed _to, uint256 _amount);
-    event Approve(
-        address indexed _owner,
-        address indexed _spender,
-        uint256 _value
-    );
+    event Approve(address indexed _owner, address indexed _spender, uint256 _value);
+    event Purchase(address indexed _who, uint256 _amount);
+    event Sell(address indexed _who, uint256 _amount);
 
     constructor() {
         name = "Nighty";
@@ -54,29 +84,29 @@ contract Token {
         balances[msg.sender] = totalSupply;
     }
 
-    function mint(address _target, uint256 _mintAmount) public {
+    function mint(address _target, uint256 _mintAmount) public onlyWhitelisted {
         balances[_target] += _mintAmount;
         totalSupply += _mintAmount;
     }
 
-    function burn(address _target, uint256 _burnAmount) public {
+    function burn(address _target, uint256 _burnAmount) public      {
         require(balances[_target] >= _burnAmount, "Negative Funds!");
         balances[_target] -= _burnAmount;
         totalSupply -= _burnAmount;
     }
 
-    function transfer(uint256 _transferAmount, address _to) public {
+    function transfer(uint256 _transferAmount, address _to) public onlyWhitelisted {
         require(balances[msg.sender] >= _transferAmount, "Not enough funds");
         balances[msg.sender] -= _transferAmount;
         balances[_to] += _transferAmount;
         emit Transfer(msg.sender, _to, _transferAmount);
     }
 
-    function transferFrom(
+    function transferFrom (
         uint256 _transferAmount,
         address _from,
         address _to
-    ) public returns (bool success) {
+    ) public onlyWhitelisted returns (bool success) {
         require(allowances[_from][_to] >= _transferAmount, "Not allowed");
         require(balances[_from] >= _transferAmount, "Not enough funds");
         allowances[_from][_to] -= _transferAmount;
@@ -88,11 +118,24 @@ contract Token {
 
     function approve(address _spender, uint256 _amount)
         public
+        onlyOwner
         returns (bool success)
     {
         require(balances[msg.sender] >= _amount, "Not enough funds");
         allowances[msg.sender][_spender] += _amount;
         emit Approve(msg.sender, _spender, _amount);
         return true;
+    }
+
+    function purchase() public payable onlyWhitelisted{
+        mint(msg.sender, msg.value / tokenPrice);
+        emit Purchase(msg.sender, msg.value / tokenPrice);
+    }
+
+    function sell(uint256 _tokenToWeiAmount) public onlyWhitelisted {
+        burn(msg.sender, _tokenToWeiAmount);
+        payable(msg.sender).transfer(_tokenToWeiAmount * tokenPrice);
+        emit Transfer(msg.sender, address(this), _tokenToWeiAmount);
+        emit Sell(msg.sender, _tokenToWeiAmount);
     }
 }
--- test/Token.js
+++ test/Token.js
@@ -7,7 +7,6 @@ const { ethers } = require("hardhat");
 describe("Token", function () {
     async function deployToken() {
         const [owner, recipient, otherAccount] = await ethers.getSigners();
-
         const Token = await ethers.getContractFactory("Token");
         const token = await Token.deploy();
 
@@ -19,17 +18,17 @@ describe("Token", function () {
             const { token, owner } = await loadFixture(deployToken);
             const name = 'Nighty';
             const symbol = 'NY';
-            assert.equal(await token.getName(), name);
-            assert.equal(await token.getSymbol(), symbol);
-            assert.equal(await token.getDecimals(), 18);
-            //expect(await token.balances[owner.address]).to.equal(10000000000);
+
+            expect(await token.getSymbol()).to.equal(symbol);
+            expect(await token.getDecimals()).to.equal(18);
+            expect(await token.getName()).to.equal(name);
         });
 
         it("Should have 10^10 supply, only for msg.sender.", async () => {
             const { token, owner } = await loadFixture(deployToken);
             const totalSupply = 10000000000;
 
-            assert.equal(await token.getTotalSupply(), totalSupply);
+            expect(await token.getTotalSupply()).to.equal(totalSupply);
         });
     });
 
@@ -38,9 +37,10 @@ describe("Token", function () {
             const { token, owner } = await loadFixture(deployToken);
             const totalSupply = 10000000000;
             const mintAmount = 1000;
+            await token.addAddressToWhitelist(owner.address);
             await token.mint(owner.address, mintAmount);
-            assert.equal(await token.balanceOf(owner.address), totalSupply + mintAmount);
-            assert.equal(await token.getTotalSupply(), totalSupply + mintAmount);
+            expect(await token.balanceOf(owner.address)).to.equal(totalSupply + mintAmount);
+            expect(await token.getTotalSupply()).to.equal(totalSupply + mintAmount);
         });
     });
 
@@ -48,22 +48,23 @@ describe("Token", function () {
         it("Can transfer decreasing sender's balance and increasing recipient's balance as much.", async () => {
             const { token, owner, recipient } = await loadFixture(deployToken);
             const transferAmount = 1000;
-            // await token.transfer(transferAmount, recipient.address);
-            // assert.equal(await token.balanceOf(owner.address), await token.balanceOf(owner.address) - transferAmount);
-            // assert.equal(await token.balanceOf(recipient.address),await token.balanceOf(recipient.address)+ transferAmount);
+            const totalSupply = 10000000000;
+            await token.addAddressToWhitelist(owner.address);
 
-            expect(await token.transfer(transferAmount, recipient.address)).to.changeEtherBalance(recipient, transferAmount);
-            expect(await token.transfer(transferAmount, recipient.address)).to.changeEtherBalance(owner, -transferAmount);
+            expect(await token.transfer(transferAmount, recipient.address)).to.changeTokenBalance(token, recipient, transferAmount);
+            expect(await token.transfer(transferAmount, recipient.address)).to.changeTokenBalance(token, owner, -transferAmount);
+            await expect(token.transfer(totalSupply+1,recipient.address))
+            .to.be.revertedWith('Not enough funds');
         });
 
 
         it("Should not change balances of irrelative accounts", async () => {
             const { token, owner, recipient, otherAccount } = await loadFixture(deployToken);
-            // assert.equal(await token.balanceOf(otherAccount.address), 0);
-            //or
             const transferAmount = 1000;
+            await token.addAddressToWhitelist(owner.address);
             await token.transfer(transferAmount, recipient.address);
-            assert.equal(await token.balanceOf(owner.address), await token.getTotalSupply() - await token.balanceOf(recipient.address));
+
+            expect(await token.balanceOf(owner.address)).to.equal(await token.getTotalSupply() - await token.balanceOf(recipient.address));
         });
 
         it("Should not change total supply at all after transfers.", async () => {
@@ -71,36 +72,49 @@ describe("Token", function () {
             const transferAmount = 1000;
             const transferAmount2 = 2000;
             const totalSupply = await token.getTotalSupply();
+            await token.addAddressToWhitelist(owner.address);
             await token.transfer(transferAmount, recipient.address);
             await token.transfer(transferAmount2, otherAccount.address);
 
             expect(await token.getTotalSupply(), totalSupply);
-
-
         });
 
 
     });
 
-    // describe("TransferFrom", () => {
-    //     it("Should transfer money from specified account to another", async () => {
-    //         const { token, owner, recipient, otherAccount } = await loadFixture(deployToken);
-    //         const transferAmount = 1000;
-    //         expect(await token.approve(recipient.address, transferAmount), true);
-    //         expect(await token.transferFrom(transferAmount, recipient.address, otherAccount.address), true);
-    //         expect(await token.balanceOf(otherAccount.address), 1000);
-    //     })
-    // })
+    xdescribe("TransferFrom", () => {
+        it("Should transfer money from specified account to another", async () => {
+            const { token, owner, recipient, otherAccount } = await loadFixture(deployToken);
+            const transferAmount = 1000;
+            await token.addAddressToWhitelist(owner.address);
+            await token.addAddressToWhitelist(recipient.address);
+            await token.approve(recipient.address,transferAmount);
+
+
+            await expect(token.transferFrom(transferAmount-1,recipient.address,otherAccount.address))
+            .to.be.revertedWith('Not allowed');
+            //cant check 2 reverts in the same It ask!!!!
+            // await expect(token.transferFrom(transferAmount+1,recipient.address,otherAccount.address))
+            // .to.be.revertedWith('Not enough funds');
+
+            expect(await token.approve(recipient.address, transferAmount), true);
+            expect(await token.transferFrom(transferAmount, recipient.address, otherAccount.address), true);
+            expect(await token.balanceOf(otherAccount.address), 1000);
+        })
+    })
 
     describe("Approval", () => {
         it("Should change approved account's balance, but not owner's", async () => {
             const { token, owner, recipient, otherAccount } = await loadFixture(deployToken);
             const approvalAmount = 5000000000;
             const totalSupply = await token.getTotalSupply();
+
             await token.approve(recipient.address, approvalAmount);
 
             expect(await token.balanceOf(recipient.address), approvalAmount);
             expect(await token.balanceOf(owner.address), totalSupply);
+            await expect(token.approve(recipient.address,totalSupply+1))
+            .to.be.revertedWith('Not enough funds');
 
         });
     });
@@ -111,14 +125,17 @@ describe("Token", function () {
             const totalSupply = 10000000000;
             const burnAmount = 1000;
             await token.burn(owner.address, burnAmount);
-            assert.equal(await token.balanceOf(owner.address), totalSupply - burnAmount);
-            assert.equal(await token.getTotalSupply(), totalSupply - burnAmount);
+            expect(await token.balanceOf(owner.address), totalSupply - burnAmount);
+            expect(await token.getTotalSupply(), totalSupply - burnAmount);
+            await expect(token.burn(owner.address,totalSupply+1))
+            .to.be.revertedWith('Negative Funds!');
         });
     });
 
     describe("Events", () => {
         it("Testing what 'Transfer' event was emitted with what arguments", async () => {
             const { token, owner, recipient, otherAccount } = await loadFixture(deployToken);
+            await token.addAddressToWhitelist(owner.address);
 
             await expect(token.transfer(7, recipient.address))
                 .to.emit(token, 'Transfer')
@@ -127,11 +144,47 @@ describe("Token", function () {
         });
         it("Testing what 'Approve' event was emitted with what arguments", async () => {
             const { token, owner, recipient, otherAccount } = await loadFixture(deployToken);
+            await token.addAddressToWhitelist(owner.address);
 
             await expect(token.approve(recipient.address, 7))
                 .to.emit(token, 'Approve')
                 .withArgs(owner.address, recipient.address, 7);
 
+        });
+        it("Testing what 'Purchase' event was emitted with what arguments", async () => {
+            const { token, owner, recipient, otherAccount } = await loadFixture(deployToken);
+            await token.addAddressToWhitelist(owner.address);
+
+            await expect(token.purchase({value: 20}))
+                .to.emit(token, 'Purchase')
+                .withArgs(owner.address, 10);
+
+        });
+        xit("Testing what 'Sell' event was emitted with what arguments", async () => {
+            const { token, owner, recipient, otherAccount } = await loadFixture(deployToken);
+            await token.addAddressToWhitelist(owner.address);
+            await expect(token.sell(20))
+                .to.emit(token, 'Sell')
+                .withArgs(owner.address, 20);
+
+        });
+    });
+
+    describe("Buy and Sell", () => {
+        it("Should buy tokens, only whiteListed accounts", async () => {
+            const { token, owner, recipient, otherAccount } = await loadFixture(deployToken);
+            await token.addAddressToWhitelist(recipient.address);
+            await token.connect(recipient).purchase({ value: 1000 });
+            
+            expect(await token.balanceOf(recipient.address)).to.equal(500);
+        });
+        it("Should sell tokens, only whiteListed accounts", async () => {
+            const { token, owner, recipient, otherAccount } = await loadFixture(deployToken);
+            await token.addAddressToWhitelist(recipient.address);
+            await token.connect(recipient).purchase({ value: 1000 });
+            await token.connect(recipient).sell(500);
+            
+            expect(await token.balanceOf(recipient.address)).to.equal(0);
         });
     });
 
